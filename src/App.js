bb1f7f2a6c788e171f764bc6f226ecc3dd6e70a2
seanka <and.seanka@gmail.com>
2023-12-08 08:46:43 +0700

    Created friend list UI

--- src/App.js
+++ src/App.js
@@ -1,3 +1,44 @@
+import { initialFriends } from "./static";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  return (
+    <ul>
+      {initialFriends.map((friend) => (
+        <FriendCard friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function FriendCard({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name} </h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owe you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
 }
